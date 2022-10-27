0a98cbd407a09f31bba3980099076cf95063b511
Jakub Olbrycht <jkb.olbrycht@gmail.com>
2022-10-27 15:36:05 +0200

    Added basic React app

---
+++ reactclient/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="row min-vh-100">
+        <div className="col d-flex flex-column justify-content-center align-items-center">
+          <h1 className="text-black">Hello</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
