d733bcc6c88e407fd6a1fd15ee989acd09378929
Jakub Olbrycht <jkb.olbrycht@gmail.com>
2022-10-27 14:20:46 +0200

    Initial Commit

---
+++ aspnetserver/Data/AppDBContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace aspnetserver.Data
+{
+    internal sealed class AppDBContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=./Data/AppDB.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Post[] postToSeed = new Post[6];
+
+            for (int i = 1; i <= 6; i++)
+            {
+                postToSeed[i - 1] = new Post
+                {
+                    PostId = i,
+                    Title = $"Post {i}",
+                    Content = $"This is post {i} and it has some very interesting content. I have also liked the video and subscribed"
+                };
+            }
+
+            modelBuilder.Entity<Post>().HasData(postToSeed);
+        }
+    }
+}
