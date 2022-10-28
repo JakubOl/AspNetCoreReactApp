6a582d38e30934e3e53ee6aa285c11be149ccf45
Jakub <jkb.olbrycht@gmail.com>
2022-10-28 23:13:33 +0200

    Changed CORS origin.

--- aspnetserver/Program.cs
+++ aspnetserver/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddCors(options =>
         {
             builder.AllowAnyMethod()
             .AllowAnyHeader()
-            .WithOrigins("http://localhost:3000", "https://appname.azurestaticapps.net");
+            .WithOrigins("http://localhost:3000", "https://red-sky-0de42c303.2.azurestaticapps.net");
         });
 });
 
